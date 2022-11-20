f304fd3433846a9b1690ae2107726639aa7836c1
WhiteCmile <whitecmile@sjtu.edu.cn>
2022-11-20 01:50:30 -0800

    remake the CMakeLists.txt and modified the input of '\n'

--- build/CMakeFiles/minivim.dir/DependInfo.cmake
+++ build/CMakeFiles/minivim.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/whitecmile/Desktop/project/src/main.cpp" "/home/whitecmile/Desktop/project/build/CMakeFiles/minivim.dir/src/main.cpp.o"
+  "/home/whitecmile/Desktop/Project/src/main.cpp" "/home/whitecmile/Desktop/Project/build/CMakeFiles/minivim.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- build/CMakeFiles/minivim.dir/flags.make
+++ build/CMakeFiles/minivim.dir/flags.make
@@ -6,5 +6,5 @@ CXX_FLAGS =   -std=gnu++17
 
 CXX_DEFINES = 
 
-CXX_INCLUDES = -I/home/whitecmile/Desktop/project/include 
+CXX_INCLUDES = -I/home/whitecmile/Desktop/Project/include 
 
